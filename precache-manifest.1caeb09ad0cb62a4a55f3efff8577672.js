38ffe89addf259eeeb3229f714d46cac703ae96f
emmafattori <emmalfattori@gmail.com>
2019-11-28 10:51:27 -0500

    Updates

---
+++ precache-manifest.1caeb09ad0cb62a4a55f3efff8577672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f257d8b727211ecfca87128d38e60ec",
+    "url": "/gh-pages-react-app/index.html"
+  },
+  {
+    "revision": "ba5ab71f02e9224ac9cf",
+    "url": "/gh-pages-react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "566977cd107981fa4003",
+    "url": "/gh-pages-react-app/static/js/2.f0e4f202.chunk.js"
+  },
+  {
+    "revision": "ba5ab71f02e9224ac9cf",
+    "url": "/gh-pages-react-app/static/js/main.b29fa08e.chunk.js"
+  },
+  {
+    "revision": "1f66ab8657c2648256e9",
+    "url": "/gh-pages-react-app/static/js/runtime-main.a2a3dedc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/gh-pages-react-app/static/media/logo.25bf045c.svg"
+  }
+]);
