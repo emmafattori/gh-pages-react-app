25a5b9432e2a9731bbe342f566de54d5fbf86451
emmafattori <emmalfattori@gmail.com>
2019-11-28 10:30:29 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h3>Welcome to Emma's App</h3>
         </a>
       </header>
     </div>
